b8d377e98a578d9a2f1bea62d5b0092f63bc1a7c
DA M1 65 <dam165@DAs-MacBook-Air.local>
2022-06-30 14:28:44 +0200

    post

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,8 +7,9 @@ import { AppComponent } from './app.component';
 import { AuthComponent } from './components/auth/auth.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { UsersComponent } from './components/users/users.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { UsersComponent } from './components/users/users.component';
+
 
 @NgModule({
   declarations: [
