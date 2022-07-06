cad9c3fc8d2ee1cba86d4ac8727d5ffe0d76a5e9
unknown <shaggybelco1@gmail.com>
2022-07-06 23:48:12 +0200

    working

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -3,59 +3,91 @@ import { Component, OnInit } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCard } from '@angular/material/card';
 import { ProfileService } from 'src/app/services/profile.service';
-
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  form: FormGroup = new FormGroup({
+    userid: new FormControl(''),
+    message: new FormControl(''),
+  });
 
-  constructor(private profile: ProfileService) { 
+  msgto: string = '';
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private profile: ProfileService
+  ) {}
+
+  name: any = {};
+  messages: any = {};
+  posting: any = {};
+
+  ngOnInit(): void {
+    console.log(localStorage.getItem('user_id'));
+    this.profile
+      .viewPost(localStorage.getItem('user_id'))
+      .subscribe((prof: any) => {
+        this.posting = prof;
+        const j = prof.length;
+        console.log(this.posting);
+
+        for (let i = 0; i < prof.length; i++) {
+          console.log(
+            (this.name = prof[i].name),
+            (this.messages = prof[i].message)
+          );
+        }
+      });
   }
 
+  get f() {
+    return this.form.controls;
+  }
+
+  post() {
+    let postdata = {
+      data: {
+        userid: localStorage.getItem('user_id'),
+        message: this.form.value.message,
+      },
+    };
+
+    if(this.form.invalid){
+      alert("can not post empty text");
+      return;
+    }else if(postdata.data.userid != '' && postdata.data.message != ''){
+      console.log('it does nothing', postdata.data);
+      this.profile.post(postdata.data).subscribe(
+      (data: any) =>{
+        alert('posted');
+        window.location.reload();
+       },
+       (err)=>{
+        alert("failed to post");
+       });
+      }
+    }
   
-  
-  addImage(){
+
+  addImage() {
     let input = document.createElement('input');
     input.type = 'file';
-    input.onchange = _=>{
+    input.onchange = (_) => {
       let files = input.files;
       console.log(files);
     };
     input.click();
   }
 
-  name: any = {};
-  message: any = {}
-  posting: any = {}
-  
-  ngOnInit(): void {
-    console.log(localStorage.getItem("user_id"));
-    this.profile.viewPost(localStorage.getItem("user_id")).subscribe(
-      (prof: any) =>{
-        
-        this.posting = prof;
-        const j = prof.length;
-        console.log(this.posting);
-        
-        for (let i = 0; i < prof.length; i++) {
-         
-          console.log( this.name = prof[i].name,
-            this.message = prof[i].message);
-        }
-        
-        
-        
-      }
-    )
-
-    
-  }
-
-  getUserId(index:any){
-    localStorage.setItem('allpost',JSON.stringify(this.name[index]))
-  }
-
+ 
 }
--- frontend/src/app/services/profile.service.ts
+++ frontend/src/app/services/profile.service.ts
@@ -6,11 +6,15 @@ import { Injectable } from '@angular/core';
 })
 export class ProfileService {
 
-  baseurl:string = "http://localhost:5050";
+  baseurl:string = "http://localhost:5050/news";
 
   constructor(private view: HttpClient) { }
 
   viewPost(id: any){
-    return this.view.get(`${this.baseurl}/news/getone/${id}`);
+    return this.view.get(`${this.baseurl}/getone/${id}`);
+  }
+
+  post(body: any){
+    return this.view.post(`${this.baseurl}/poststatus`, body);
   }
 }
