7631b5c771fb3b1e3e45e18e17e2837596a2943b
unknown <shaggybelco1@gmail.com>
2022-07-08 04:42:33 +0200

    working

--- frontend/src/app/services/profile.service.ts
+++ frontend/src/app/services/profile.service.ts
@@ -6,15 +6,20 @@ import { Injectable } from '@angular/core';
 })
 export class ProfileService {
 
-  baseurl:string = "http://localhost:5050/news";
+  baseurl:string = "http://localhost:5050";
 
   constructor(private view: HttpClient) { }
 
   viewPost(id: any){
-    return this.view.get(`${this.baseurl}/getone/${id}`);
+    return this.view.get(`${this.baseurl}/news/getone/${id}`);
   }
 
   post(body: any){
-    return this.view.post(`${this.baseurl}/poststatus`, body);
+    return this.view.post(`${this.baseurl}/news/poststatus`, body);
   }
+
+  getPic(id: any){
+    return this.view.get(`${this.baseurl}/user/`)
+  }
+
 }
