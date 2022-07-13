abcb0076ae15d300c2e424497eaafb8caa915871
DA M1 65 <dam165@DAs-MacBook-Air.local>
2022-07-13 15:03:43 +0200

    work

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     content: ['./src/**/*.{html,js,ts}'],
     plugins: [require('daisyui')],
-    darkMode: false,
+    darkMode: true,
     daisyui: {
         themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
       },
