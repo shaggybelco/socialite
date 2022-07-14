d89e99b86a1f7d46c1c9ea141de49ae228962b40
DA M1 65 <dam165@DAs-MacBook-Air.local>
2022-07-14 12:54:18 +0200

    front

--- frontend/src/app/components/newsfeed/newsfeed.component.ts
+++ frontend/src/app/components/newsfeed/newsfeed.component.ts
@@ -32,82 +32,117 @@ export class NewsfeedComponent implements OnInit {
   posting: any = {};
   imgurl: any = {};
   imgpost: any={};
+  imgdate: any={};
 
   ngOnInit(): void {
-    // console.log(localStorage.getItem('user_id'));
-    // this.profile
-    //   .viewPost(localStorage.getItem('user_id'))
-    //   .subscribe((prof: any) => {
-    //     this.posting = prof;
-    //     const j = prof.length;
-    //     console.log(this.posting);
+    console.log(localStorage.getItem('user_id'));
+    this.profile
+      .viewPost(localStorage.getItem('user_id'))
+      .subscribe((prof: any) => {
+        this.posting = prof;
+        const j = prof.length;
+        console.log(this.posting);
 
-    //     for (let i = 0; i < prof.length; i++) {
-    //       console.log(
-    //         (this.name = prof[i].name),
-    //         (this.messages = prof[i].message)
-    //       );
-    //     }
-    //   });
+        for (let i = 0; i < prof.length; i++) {
+          console.log(
+            (this.name = prof[i].name),
+            (this.messages = prof[i].message)
+          );
+        }
+      });
 
-    //   this.profile
-    //   .getPic(localStorage.getItem('user_id'))
-    //   .subscribe((imgstat: any) => {
-    //     this.imgpost = imgstat;
-    //     const j = imgstat.length;
-    //     console.log(imgstat);
+      this.profile
+      .getPic(localStorage.getItem('user_id'))
+      .subscribe((imgstat: any) => {
+        this.imgpost = imgstat;
+        const j = imgstat.length;
+        console.log(imgstat);
 
-    //     for (let i = 0; i < imgstat.length; i++) {
-    //       console.log(
-    //         (this.name = imgstat[i].name),
-    //         (this.messages =imgstat[i].caption),
-    //         (this.imgurl = imgstat[i].image)
-    //       );
-    //     }
-    //   });
+        for (let i = 0; i < imgstat.length; i++) {
+          console.log(
+            (this.name = imgstat[i].name),
+            (this.messages =imgstat[i].caption),
+            (this.imgurl = imgstat[i].image),
+            (this.imgdate = imgstat[i].date)
+          );
+        }
+      });
   }
+
+  
   form: FormGroup = new FormGroup({
     userid: new FormControl(''),
     message: new FormControl(''),
+    date: new FormControl('')
   });
 
   post() {
-    let postdata = {
-      data: {
-        userid: localStorage.getItem('user_id'),
-        message: this.form.value.message,
-      },
-    };
+    // let postdata = {
+    //   data: {
+    //     userid: localStorage.getItem('user_id'),
+    //     message: this.form.value.message,
+    //   },
+    // };
 
-    if(this.form.invalid){
-      alert("can not post empty text");
-      return;
-    }else if(postdata.data.userid != '' && postdata.data.message != ''){
-      console.log('it does nothing', postdata.data);
-      this.profile.post(postdata.data).subscribe(
-      (data: any) =>{
-        alert('posted');
-        window.location.reload();
-       },
-       (err)=>{
-        alert("failed to post");
-       });
-      }
+    // if(this.form.invalid){
+    //   alert("can not post empty text");
+    //   return;
+    // }else if(postdata.data.userid != '' && postdata.data.message != ''){
+    //   console.log('it does nothing', postdata.data);
+    //   this.profile.post(postdata.data).subscribe(
+    //   (data: any) =>{
+    //     alert('posted');
+    //     window.location.reload();
+    //    },
+    //    (err)=>{
+    //     alert("failed to post");
+    //    });
+    //   }
+
+        const formdata = new FormData();
+   
+        this.form.get('userid')?.setValue(localStorage.getItem('user_id'));
+        
+        this.form.value.date = this.todayDate;
+        formdata.append('userid', this.form.value.userid);
+        formdata.append('caption', this.form.value.message)
+        formdata.append('myfile',this.files);
+        formdata.append('date',this.form.value.date);
+  
+       
+        console.log('it does nothing', formdata);
+  
+        this.uploadingPic.uploading(formdata).subscribe(
+          (data: any) => {
+            alert('posted');
+            console.log(data);
+            window.location.reload();
+          },
+          (err) => {
+            alert('failed to post');
+          }
+        );
+
     }
   
+    todayDate : Date = new Date();
+
     files: any = {};
     addImage() {
     
+      
       let input = document.createElement('input');
       const formdata = new FormData();
       input.type = 'file';
       input.onchange = (_) => {
         this.files = input.files?.item(0);
         this.form.get('userid')?.setValue(localStorage.getItem('user_id'));
-  
+        
+        this.form.value.date = this.todayDate;
         formdata.append('userid', this.form.value.userid);
         formdata.append('caption', this.form.value.message)
         formdata.append('myfile',this.files);
+        formdata.append('date',this.form.value.date);
   
        
         console.log('it does nothing', formdata);
