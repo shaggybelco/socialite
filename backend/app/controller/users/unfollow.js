7e086b70fa2978ce320c3e27af490231115284ec
DA_M1_31 <da_m1_31@Academy-learners-MacBook-Air.local>
2022-07-22 08:37:05 +0200

    working

--- backend/app/controller/users/unfollow.js
+++ backend/app/controller/users/unfollow.js
@@ -1,44 +1,40 @@
+
 const pool = require("../../config/database");
-const express = require("express");
-const app = express();
-const bodyparser = require('body-parser');
 
-exports.unfollow = (req, res) => {
-    const { id, userid} = req.body;
-  
-    pool.query(
-      'update users set follow = array_remove(follow, $1) where id = $2;',
-      [userid, parseInt(id)],
-      (error, results) => {
-        if (error) {
-          throw error;
-        }
-        res.status(200).send(`User ${userid} modified with ID: ${id}`);
-      }
-    );
+//unFollow Function
 
-      //This Will Only Work For NewsFeed
+exports.toFollow = async (req, res) => {
+    const { userid, followid,followStatus } = req.body;
 
-      pool.query(
-        'DELETE FROM follow  WHERE userid = $1 AND followid = $2;',
-        [id,userid],
-        (err) => {
-            if (err) {
-                flag = 0; //If user is not inserted is not inserted to database assigning flag as 0/false.
-                console.error(err);
-                return res.status(500).json({
-                    error: "Database error",
-                });
-            } else {
-                flag = 1;
-               
+    try {
+        
+            //Inserting data into the database
+            pool.query(
+                `DELETE FROM follow WHERE userid = $1, followid = $2, followStatus = $3`,
+                [userid,followid,followStatus],
+                (err) => {
+                    if (err) {
+                        flag = 0; //If user is not inserted is not inserted to database assigning flag as 0/false.
+                        console.error(err);
+                        return res.status(500).json({
+                            error: "Database error",
+                        });
+                    } else {
+                        flag = 1;
+                        res
+                            .status(200)
+                            .send({ message: `User called with ID  ${followid} has been unfollowed by user with ID ${userid}` });
+                    }
+                }
+            );
+            if (flag) {
             }
         }
-    );
 
-  
-
-      //End here
-
-
-  };
+    catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: "Database error while following user!", //Database connection error
+        });
+    }
+};
